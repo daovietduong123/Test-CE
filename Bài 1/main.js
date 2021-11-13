7f0e2b587af748a234a0a25e56c4b9ce159cc403
Dao Duong <daoduongtn2000@gmail.com>
2021-11-13 20:57:53 +0700

    duong nop bai test

---
+++ Bài 1/main.js
@@ -0,0 +1,18 @@
+var arr = [
+  'Dao',
+  'Viet',
+  'Duong'
+];
+
+var lgth = 0;
+var longest;
+
+for (var i = 0; i < arr.length; i++) {
+  if (arr[i].length > lgth) {
+    var lgth = arr[i].length;
+    longest = arr[i];
+  }
+}
+var newArr = [];
+newArr.push(longest)
+console.log(newArr);
